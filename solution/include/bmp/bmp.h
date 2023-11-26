42bbe9248bada6a1de6b0b20d564239423b0772d
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-11-27 01:55:11 +0300

    ура, фиксим тесты

--- solution/include/bmp/bmp.h
+++ solution/include/bmp/bmp.h
@@ -1,9 +1,9 @@
 #ifndef IMAGE_TRANSFORMER_BMP_H
 #define IMAGE_TRANSFORMER_BMP_H
 
-#include <stdio.h>
 #include "image/image.h"
 #include "file_work/file_work.h"
+#include <stdio.h>
 
 enum input_state from_bmp_to_image(FILE* in, struct image* image );
 enum output_state from_image_to_bmp(FILE * out, struct image * image);
